0a45bfb59d6d1a5a4138aa8f69ce180e01053fda
jsto <jstoltz69600@gmail.com>
2025-07-28 16:38:53 +0200

    New

--- Remove-OldAntiRansomwareSnapshots.ps1
+++ Remove-OldAntiRansomwareSnapshots.ps1
@@ -1,10 +1,10 @@
-# Importer le module NetApp PowerShell Toolkit
-Import-Module DataONTAP
-
 # Informations de connexion
-$NetAppCluster = "adresse_ip_ou_nom_de_la_baie"
-$Username = "votre_nom_utilisateur"
-$Password = "votre_mot_de_passe"
+$NetAppCluster = "192.168.0.101"
+$Username = "admin"
+$Password = "Netapp1!"
+$domain = "DEMO"
+$dnsdomain = "demo.netapp.com"
+$domainadmin = "Administrator@"+$domain
 
 # Convertir le mot de passe en SecureString
 $SecurePassword = ConvertTo-SecureString -String $Password -AsPlainText -Force
@@ -15,35 +15,25 @@ $Credential = New-Object -TypeName System.Management.Automation.PSCredential -Ar
 # Se connecter à la baie NetApp
 Connect-NcController -Name $NetAppCluster -Credential $Credential
 
-# Définir la date limite pour les snapshots à supprimer (plus de 15 jours)
-$DateLimit = (Get-Date).AddDays(-15)
+#Récuper la version de la baie
+$Systver = Get-NcSystemVersion
+
+if ($Systver.Version -like "*9.16.1*" ) {
+    Invoke-Ncssh "options -option-name arw.snap.retain.hours.after.clear.suspect.false.alert -option-value 4"
+    Write-Host " Passage de la suppression du snap antiransomware apres faux positif à 4h"
+}
 
 # Récupérer tous les volumes
 $Volumes = Get-NcVol
 
-#Passage de la suppression du snap antiransomware apres faux positif a 4h
-Invoke-Ncssh "options -option-name arw.snap.retain.hours.after.clear.suspect.false.alert -option-value 4"
-Write-Host " Passage de la suppression du snap antiransomware apres faux positif a 4h"
-
-
 foreach ($Volume in $Volumes) {
-    # Récupérer tous les snapshots pour chaque volume
-    $Snapshots = Get-NcSnapshot -Volume $Volume.Name
-    # Déclarer le faux positif sur le volume
-    Invoke-Ncssh "anti-ransomware volume attack clear-suspect -vserver $($Volume.vserver) -volume $($Volume.Name) -false-positive true"
-    Write-Host " Volume $($Volume.Name) declaration en faux positif"
-
-    foreach ($Snapshot in $Snapshots) {
-        # Vérifier si le snapshot est un snapshot anti-ransomware et s'il est plus ancien que la date limite
-        if ($Snapshot.Name -like "*Anti_ransomware*" -and $Snapshot.Created -lt $DateLimit) {
-            # Supprimer le snapshot
-            Invoke-Ncssh "snapshot delete -vserver $($Volume.vServer) -Volume $($Volume.Name) -Snapshot $($Snapshot.Name) -ignore-owners true -force true" 
-            Write-Host "Snapshot $($Snapshot.Name) sur le volume $($Volume.Name) a été supprimé."
-        }
+    
+# Si le volume a Antiransomware d'activé et n'est pas un volume root alors supprime l'alerte Antiransomware
+    if ($Volume.AntiRansomware -like "DataONTAP.C.Types.Volume.AntiRansomware" -and $Volume.JunctionPath -ne '/'){
+        Remove-NcAntiRansomwareSuspect -Volume $Volume.Name -vserverContex $Volume.vserver -Confirm:$false
+        Write-Host " Volume $($Volume.Name) declaration en faux positif et snapshot sera supprime dans 4 (a partir de 9.16) ou 24h"
     }
+       
 }
 
-# Se déconnecter de la baie NetApp
-Disconnect-NcController -Name $NetAppCluster
-
-Write-Host "Opération terminée."
+Write-Host "Operation terminee."
