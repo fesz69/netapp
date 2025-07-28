012eb5dae968e7920d36b6c518e1725ca4e8afb0
jsto <jstoltz69600@gmail.com>
2025-07-28 16:38:14 +0200

    New Powershell script

--- Remove-OldAntiRansomwareSnapshots.ps1
+++ Remove-OldAntiRansomwareSnapshots.ps1
@@ -21,15 +21,23 @@ $DateLimit = (Get-Date).AddDays(-15)
 # Récupérer tous les volumes
 $Volumes = Get-NcVol
 
+#Passage de la suppression du snap antiransomware apres faux positif a 4h
+Invoke-Ncssh "options -option-name arw.snap.retain.hours.after.clear.suspect.false.alert -option-value 4"
+Write-Host " Passage de la suppression du snap antiransomware apres faux positif a 4h"
+
+
 foreach ($Volume in $Volumes) {
     # Récupérer tous les snapshots pour chaque volume
     $Snapshots = Get-NcSnapshot -Volume $Volume.Name
+    # Déclarer le faux positif sur le volume
+    Invoke-Ncssh "anti-ransomware volume attack clear-suspect -vserver $($Volume.vserver) -volume $($Volume.Name) -false-positive true"
+    Write-Host " Volume $($Volume.Name) declaration en faux positif"
 
     foreach ($Snapshot in $Snapshots) {
         # Vérifier si le snapshot est un snapshot anti-ransomware et s'il est plus ancien que la date limite
-        if ($Snapshot.Name -like "*antiransomware*" -and $Snapshot.Created -lt $DateLimit) {
+        if ($Snapshot.Name -like "*Anti_ransomware*" -and $Snapshot.Created -lt $DateLimit) {
             # Supprimer le snapshot
-            Remove-NcSnapshot -Volume $Volume.Name -Snapshot $Snapshot.Name -Confirm:$false
+            Invoke-Ncssh "snapshot delete -vserver $($Volume.vServer) -Volume $($Volume.Name) -Snapshot $($Snapshot.Name) -ignore-owners true -force true" 
             Write-Host "Snapshot $($Snapshot.Name) sur le volume $($Volume.Name) a été supprimé."
         }
     }
